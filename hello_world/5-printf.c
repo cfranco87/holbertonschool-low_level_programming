ea5acd7f8ca746edf0109304d22dbcd6f8036d6a
cfranco87 <carlosfrancoart@gmail.com>
2023-06-15 06:44:52 -0700

    o

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints 
- * Return: always 0
+ *main-prints
+ *Return:always 0
  */
 
-Printf("with proper grammar, but the outcome is a piece of art");
-{
-	char string[] = "with proper grammar, but the outcome is a piece of art"	printf("%d|n", string);
-}	
+int main(void)
+char string[] = "with proper grammar, but the outcome is a piece of art"
+	printf("%s|n", string);
+	return (0);
+	}
+
