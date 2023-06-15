929aecf205479598946b521c062d43f333710bca
cfranco87 <carlosfrancoart@gmail.com>
2023-06-15 09:47:42 -0700

    d

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
 
 /**
  * main - functions
@@ -8,11 +7,11 @@
 
 int main(void)
 {
-		printf("Size of char: %zu bytes\n", sizeof(charType));
-		printf("Size of int: %zu bytes\n", sizeof(intType));
-		printf("Size of long int: %zu bytes\n", sizeof(longintType));
-		printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
-		printf("Size of float: %zu bytes\n", sizeof(floatType));
+		printf("Size of a char: %zu bytes\n", sizeof(charType));
+		printf("Size of an int: %zu bytes\n", sizeof(intType));
+		printf("Size of an long int: %zu bytes\n", sizeof(longintType));
+		printf("Size of an long long int: %zu bytes\n", sizeof(longlongintType));
+		printf("Size of a float: %zu bytes\n", sizeof(floatType));
 
 		Return(0);
 }
