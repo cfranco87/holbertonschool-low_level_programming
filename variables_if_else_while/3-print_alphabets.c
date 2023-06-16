140ce610e48a0a59ec9cde3dcf0547ff98a3f777
cfranco87 <carlosfrancoart@gmail.com>
2023-06-16 09:26:45 -0700

    a

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet
+ * Return: always (success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	while (alphabet = 'A'; alphabet <= 'Z'; alphabet++)                    
+	{
+		putchar(alphabet);                               
+   	}
+
+	putchar('\n');
+	return (0);
+}
