cef7952899cd611d071902bb22be60b80ddf8ef7
cfranco87 <carlosfrancoart@gmail.com>
2023-06-16 05:36:01 -0700

    s

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 
 	else if(ld == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n , ld);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 
 	else(ld < 6 && ld != 0)
