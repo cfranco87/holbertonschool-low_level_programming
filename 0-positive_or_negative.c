23f7f936d2a1b3157c383d5ee8d4ef734eb6d6dd
cfranco87 <carlosfrancoart@gmail.com>
2023-06-15 19:12:53 -0700

    k

--- 0-positive_or_negative.c
+++ 0-positive_or_negative.c
@@ -10,10 +10,10 @@ int main(void)
 		printf("is positive");
 	else if (n < 0)
 		printf("is negative");
-	else 
+	else
 		printf("is zero");
-	
-	
+
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
