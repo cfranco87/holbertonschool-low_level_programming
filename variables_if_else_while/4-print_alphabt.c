db887952c8fdce9a8ec94f665ed6d61831485ac1
cfranco87 <carlosfrancoart@gmail.com>
2023-06-16 10:32:52 -0700

    r

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - no eq
+ * Return: always (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
