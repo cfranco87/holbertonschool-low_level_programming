68c8296e5158521034deda96f9d153f540f12d34
cfranco87 <carlosfrancoart@gmail.com>
2023-06-18 08:01:04 -0700

    y

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - reverse
+ * Return: always (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a <= 'a'; a--)
+	{
+		putchar (a);
+	}
+
+		putchar('\n');
+		return (0);
+}
