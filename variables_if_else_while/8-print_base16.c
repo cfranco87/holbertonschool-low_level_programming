b614db02f22c6909ef3870d94a623a325a31c9ee
cfranco87 <carlosfrancoart@gmail.com>
2023-06-18 13:57:02 -0700

    u

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- base
+ * Return: always (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	{
+		for (x = 0, x <= 16, x++)
+		{ 
+			putchar(x);
+			putchar $((10#16));
+		}
+	}
+
+		putchar('\n');
+		return (0);
+}
