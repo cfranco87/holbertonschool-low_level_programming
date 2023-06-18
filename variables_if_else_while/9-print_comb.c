0833d5f7bc0bae712327ac742383e22ebbc65dc5
cfranco87 <carlosfrancoart@gmail.com>
2023-06-18 16:34:38 -0700

    i

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+
+/**
+ * main- last
+ * Return: always (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+	}
+
+
+	putchar('\n');
+	return (0);
+}
