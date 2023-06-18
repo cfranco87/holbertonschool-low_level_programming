a69041ae4c2418302a690bb3922ca6766e0d6cd2
cfranco87 <carlosfrancoart@gmail.com>
2023-06-17 21:54:12 -0700

    t

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -numbers
+ * Return: always (success)
+ */
+
+int main(void)
+{
+	for(int i = 0; i <= 9, i++)
+		putchar('0' + i);
+		putchar('\n');
+		return (0);
+}
