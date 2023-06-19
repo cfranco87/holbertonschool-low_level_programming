6166168427cce1655b236513a1279f91500b1a6a
cfranco87 <carlosfrancoart@gmail.com>
2023-06-19 06:08:25 -0700

    r

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - loops
+ * Return: Always
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+
+	return (0);
+}
