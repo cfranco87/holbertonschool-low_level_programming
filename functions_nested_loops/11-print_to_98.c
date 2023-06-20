6bd56c12b83ac6e123888c2eea5c5c9aaf7eb290
cfranco87 <carlosfrancoart@gmail.com>
2023-06-20 16:47:37 -0700

    z

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - numbers
+ * @n: - number
+ * Return: always success
+ */
+
+void print_to_98(int n)
+{
+	int last = 98;
+
+	if (n < 98)
+	{
+		for (n = n; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+
+	}
+	else
+	{
+		for (n = n; n < 98; n--);
+
+				{
+				printf("%d, ", n);
+				}
+	}
+
+	printf("%d\n", last);
+}
