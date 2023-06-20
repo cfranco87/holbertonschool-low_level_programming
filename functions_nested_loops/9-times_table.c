bb5f364364e2d0fec337b7ecd0e93459832dc772
cfranco87 <carlosfrancoart@gmail.com>
2023-06-20 10:00:55 -0700

    i

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,16 +7,28 @@
 
 void times_table(void)
 {
-	int x;
+	int x, y, z;
 
 	for (x = 0; x <= 9; x++)
 	{
-		_putchar(x + '0');
-		if (x < 9)
+		_putchar('0');
+
+		for (y = 1; y <= 9; y++)
 		{
 			_putchar(',');
 			_putchar(' ');
+
+			z = (x * y);
+
+			if (x <= 9)
+				_putchar(' ');
+			else
+				_putchar((z / 10) + '0');
+
+			_putchar((z % 10) + '0');
+
 		}
+
 		_putchar('\n');
 
 	}
