dba2fae1f22bb8f1801a1802ed3feecedcac456f
cfranco87 <carlosfrancoart@gmail.com>
2023-06-19 17:33:17 -0700

    g

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - multiple
+ * Return: always (sucess)
+ */
+
+void print_alphabet_x10(void);
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x * 10);
+
+		_putchar('\n');
+}
