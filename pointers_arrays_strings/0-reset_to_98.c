da07195d0a2f2a9fce45aeae0924b173689e590e
cfranco87 <carlosfrancoart@gmail.com>
2023-06-21 17:12:43 -0700

    e

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - 98 is the number
+ * @n: number
+ * Return: always success
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+	int p;
+
+	n = 98;
+	p = &n;
+	*p = 98
+
+	_putchar(n);
+}
