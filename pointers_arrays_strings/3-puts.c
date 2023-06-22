5986b670c625483c4b35e673f3bf0101386fe3d6
cfranco87 <carlosfrancoart@gmail.com>
2023-06-22 10:05:38 -0700

    f

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdout.h>
+
+
+/**
+ *_puts - putting things in
+ *@str: string
+ *Return: always success
+ */
+
+void _puts(char *str)
+{
+	char  c;
+
+	for char(c = 0; str[c]; c++);
+	{
+		_puts(str[c]);
+		_putchar('\n')
+	}
+}
