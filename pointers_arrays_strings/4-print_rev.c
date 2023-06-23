fe2a305a7ccbfd4f234b3768b51c889239746cb3
cfranco87 <carlosfrancoart@gmail.com>
2023-06-23 05:55:57 -0700

    f

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  *print_rev - putting things in
@@ -9,8 +10,16 @@
 
 void print_rev(char *s)
 {
-		while (*s)
-			_putchar(*s--);
-			_putchar('\n');
+	int i, len, temp;
 
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+
+		_putchar('\n');
 }
