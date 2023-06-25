85e058ccff83167bb3d6897584743136655ed8a9
cfranco87 <carlosfrancoart@gmail.com>
2023-06-25 14:15:15 -0700

    r

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@
 
 void print_array(int *a, int n)
 {
+
 	for (int i = 0; i < 5; i++)
 		a[i] = n;
 
-	int *a[] = {1, 2, 3, 4, 5};
 
 	for (int i = 0; i < 5; i++)
 		printf("a[%d] = %d\n", i, a[i]);
