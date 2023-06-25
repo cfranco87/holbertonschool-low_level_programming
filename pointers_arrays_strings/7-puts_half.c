63894a2cfdd6d1005184ae2e51bdb40f6e8f150b
cfranco87 <carlosfrancoart@gmail.com>
2023-06-24 20:55:11 -0700

    f

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *puts_half - second half of string
+ *@str: string
+ *Return: always sucess
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+	int n;
+
+	while (*(str + i) != 0)
+	{
+		for (j = 0; j < i / 2; j++)
+		{
+			j++;
+		}
+
+	_putchar(str[j]);
+	}
+
+	_putchar(n = (i - 1) / 2);
+	_putchar('\0');
+}
+
