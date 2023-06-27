f5b86a2c81a474f93baf8e996aab5dda42e6be64
cfranco87 <carlosfrancoart@gmail.com>
2023-06-26 20:19:16 -0700

    e

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reverse_array- rev array
+ *@a: array
+ *@n: anything
+ *Return: always success
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		j = i - 1;
+		j--;
+	}
+}
