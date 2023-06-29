46a48580ff98ac8d59c0584fee5b6f54f67dc0c3
cfranco87 <carlosfrancoart@gmail.com>
2023-06-29 06:55:53 -0700

    r

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,25 +9,15 @@
 
 int factorial(int n)
 {
+	int i = n;
+
 	if (n < 0)
-	{
 		return (-1);
-	}
 
-	else if (n == 0)
-	{
+	else if (n >= 0 && n <= 1)
 		return (1);
-	}
 
-	else
-	{
-		int i = 1;
+	i  *= factorial(n - 1);
 
-		for (i = 1; i <= n; i++)
-		{
-			n *= i;
-		}
-	}
-
-	return (n);
+	return (i);
 }
