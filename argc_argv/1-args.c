52a09801b4a12349785fb857417f4541d3e4e470
cfranco87 <carlosfrancoart@gmail.com>
2023-07-02 11:07:56 -0700

    d

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - open
+ *@argv: argument vector
+ *@argc: argument counter
+ *Return: always success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("arg[%d] = %s\n", i, argv[i]);
+	}
+}
