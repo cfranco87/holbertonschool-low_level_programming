6bb4f3ccb8cebdca822abd8bd4fcb1de52785f2a
cfranco87 <carlosfrancoart@gmail.com>
2023-07-01 18:17:04 -0700

    f

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - open
+ *@argv: argument vector
+ *@argc: argument counter
+ *Return: always success
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+
+	{
+		_putchar(argc);
+		_putchar('\n');
+	}
+}
