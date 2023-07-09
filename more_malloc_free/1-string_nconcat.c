1fb57cb4c967a2ba009f28d7a1f25532b1074b5a
cfranco87 <carlosfrancoart@gmail.com>
2023-07-08 18:05:31 -0700

    e

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *string;
-	int k, i = 0;
-	unsigned int j = 0;
+	unsigned int k, i = 0, j = 0;
 
 	if (s1 == NULL || s2 == NULL)
 	{
