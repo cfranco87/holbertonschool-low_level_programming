94e72255cd0ae125d29a60b976d9669781ddf797
cfranco87 <carlosfrancoart@gmail.com>
2023-07-09 18:28:11 -0700

    s

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,4 +1,15 @@
-#ifndef NAME
-#include <stdlib.h>
-#define NAME ("2-main.c")
-#endif
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
