92d989cc43a349f177eb4a2a259ada2a03423203
cfranco87 <carlosfrancoart@gmail.com>
2023-07-10 10:53:20 -0700

    s

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *init_dog - dog dog dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: who owns dog
+ *@d: davis the dog
+ *Return: success always
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d != NULL)
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
+
