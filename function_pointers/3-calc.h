00667f8dc468f025b0d3a46c48150bd8398b19d3
cfranco87 <carlosfrancoart@gmail.com>
2023-07-16 18:18:30 -0700

    s

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef CALC_H
+#define CALC_H
 
 /**
  *struct op - Struct op
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,18 +10,18 @@
 int (*get_op_func(char *s))(int, int)
 {
 
-	int (*fun)(int, int);
-	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	if (*s != '+' && *s != '-' && *s != '*' && *s != '/' && *s != '%')
-	{
-		return (NULL);
-	}
-
-	while (i > 0)
-	{
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
 
-	return (fun);
+	return (ops[i].f);
 }
