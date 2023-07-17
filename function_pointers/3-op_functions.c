f2c253aa4dc108edf8234321c3f0bd85c1a8bee9
cfranco87 <carlosfrancoart@gmail.com>
2023-07-16 18:26:21 -0700

    s

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,66 +1,66 @@
-// operations for addition, subtraction, division, module and multiplication//
 #include "3-calc.h"
 
-/**
- *op_add - addition
- *@a: variable
- *@b:variable
- * Return: the sum of a and b
- */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
+/**
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - subtraction
- * @a: variable
- * @b: variable
- * Return: subtraction of a and b
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
  */
-
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - multiplication
- * @a: variable
- * @b: variable
- * Return: multiplication of a and b
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
  */
-
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - division
- * @a: variable
- * @b: variable
- * Return: division of a and b
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
  */
-
 int op_div(int a, int b)
-if ( a == 0 || b == 0)
 {
-	_putchar("Error\n");
-	exit(100);
+	return (a / b);
 }
-else
-{
-	printf( a / b);
 
 /**
- *op_mod - modules
- *@a: variable
- *@b: variable
- *Return: remainder of modules
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
  */
-
 int op_mod(int a, int b)
 {
 	return (a % b);
