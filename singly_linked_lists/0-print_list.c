8c1a8d878116c664bd99dc1e1d8fe35c1b10318b
cfranco87 <carlosfrancoart@gmail.com>
2023-07-25 05:58:26 -0700

    s

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/**
+ *print_list - singly linked list
+ *@h: string - (malloc'ed string)
+ *Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	printf("[");
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("%s", h->str);
+		}
+		else
+		{
+			printf("[0] (nil)");
+		}
+
+		h = h->next;
+		count++;
+
+		if (h != NULL)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("]\n");
+	return (count);
+}
