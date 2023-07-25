da85e7f92a1d3698da5a23e14716468e3a2b8ae4
cfranco87 <carlosfrancoart@gmail.com>
2023-07-25 06:11:55 -0700

    s

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "list.h"
+#include "lists.h"
 
 /**
  *print_list - singly linked list
@@ -36,5 +36,6 @@ size_t print_list(const list_t *h)
 	}
 
 	printf("]\n");
+
 	return (count);
 }
