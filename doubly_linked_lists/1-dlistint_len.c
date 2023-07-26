ecb8b51d36835e97ba39897ca610a0f4f2aedcc3
cfranco87 <carlosfrancoart@gmail.com>
2023-07-26 08:12:08 -0700

    s

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -12,10 +12,12 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
+	const dlistint_t *current = h;
 
 	while (h != NULL)
 	{
-		h = h->next;
+		printf("%d\n", current->n);
+		current  = current->next;
 		count++;
 	}
 	printf("-> %lu elements\n", count);
