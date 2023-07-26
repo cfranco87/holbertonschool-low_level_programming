bea9288ec0585c558bcc9bf91676aae39dd6d504
cfranco87 <carlosfrancoart@gmail.com>
2023-07-25 19:25:43 -0700

    s

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ *print_dlistint - doubly linked list
+ *@h: string - (malloc'ed string)
+ *Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+		count++;
+	}
+		return (count);
+}
