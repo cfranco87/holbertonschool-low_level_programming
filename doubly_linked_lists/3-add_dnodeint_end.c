3d212c8079c9dfdde10001622c317716961f94cf
cfranco87 <carlosfrancoart@gmail.com>
2023-07-26 20:04:11 -0700

    s

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+
+/**
+ *add_dnodeint_end - add new node to list
+ *@head: new head of node
+ *@n: number of node
+ *Return: nodes
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head != NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}
