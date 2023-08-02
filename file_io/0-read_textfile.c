bfd6f3819e32fb1a2f3cfc0a311b6ce43b3fea5f
cfranco87 <carlosfrancoart@gmail.com>
2023-08-01 19:08:00 -0700

    s

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -12,26 +12,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
-	FILE *file = fopen(filename, "r");
+	FILE *file;
 
+	file = fopen(filename, "r");
 	if (file == NULL)
 		return (0);
 
 
-	char *buffer = (char *)malloc(letters + 1);
+	char *buffer;
 
+	buffer = (char *)malloc(letters + 1);
 	if (buffer == NULL)
 	{
 		fclose(file);
 		return (0);
 	}
 
-	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
+	ssize_t bytesRead;
 
+	bytesRead = fread(buffer, sizeof(char), letters, file);
 	fclose(file);
 
 	if (bytesRead <= 0)
@@ -42,8 +43,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer[bytesRead] = '\0';
 
-	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	ssize_t bytesWritten;
 
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 	free(buffer);
 
 	if (bytesWritten != bytesRead)
