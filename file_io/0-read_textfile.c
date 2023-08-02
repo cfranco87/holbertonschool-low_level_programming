3ad513739527591d4efc84cc1593187870e45f6b
cfranco87 <carlosfrancoart@gmail.com>
2023-08-01 18:48:51 -0700

    s

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ *read_textfile - read text
+ *@filename: file name
+ *@letters: letters
+ *Return: success
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+		return (0);
+
+
+	char *buffer = (char *)malloc(letters + 1);
+
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
+
+	fclose(file);
+
+	if (bytesRead <= 0)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytesRead] = '\0';
+
+	ssize_t bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+
+	free(buffer);
+
+	if (bytesWritten != bytesRead)
+		return (0);
+
+	return (bytesRead);
+}
+
---
+++ file_io/main.c
@@ -0,0 +1,12 @@
+#ifndef FILE_D
+#define FILE_D
+
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
