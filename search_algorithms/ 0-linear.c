59cbe6f40bde644c95c2927ab4be97fc47591c1d
cfranco87 <carlosfrancoart@gmail.com>
2023-09-10 02:36:54 +0000

    fix error for worst case 4-O

---
+++ search_algorithms/ 0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *linear_search - function that searches for a value in a sorted array of
+ *integers using the Binary search algorithm
+ *@value: value within array
+ *@size: size of the array
+ *@array: array in which will will search
+*/
+
+/*function for linear search*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    int i = 0; 
+    flag = 0; 
+
+    if(array == NULL || size == 0)
+        return;
+
+    for( ; i < size, i++)
+        return 
+
+
+    flag == 0; 
+
+}
