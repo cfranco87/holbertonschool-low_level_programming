5062bebfe3cf1bb27df43e92130279ac39351ee7
cfranco87 <carlosfrancoart@gmail.com>
2023-09-11 10:58:07 -0700

    betty issues

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -7,33 +7,31 @@
  *integers using the Binary search algorithm
  *@value: value within array
  *@size: size of the array
- *@array: array in which will will search
+ *@array: array in which we will search
 */
-
-/*function for linear search*/
 
 int linear_search(int *array, size_t size, int value)
 {
-    long unsigned int i = 0;
-    int flag = 0;
+	long unsigned int i = 0;
+	int flag = 0;
 
-    if(!value && array == NULL)
-        return (-1);
+	if (!value && array == NULL)
+		return (-1);
 
-    if (size == 0)
-        return(-1);
+	if (size == 0)
+		return (-1);
 
-    for(i = 0; i < size; i++)
-    { 
-        flag = 1;
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if(*(array + i) == value)
-        {   
-            return (i);            
-        }
-        if (flag == 0)
-        break;
-    }
+	for (i = 0; i < size; i++)
+	{
+		flag = 1;
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (*(array + i) == value)
+		{
+			return (i);
+		}
+		if (flag == 0)
+		break;
+	}
 
-    return (-1);
+	return (-1);
 }
