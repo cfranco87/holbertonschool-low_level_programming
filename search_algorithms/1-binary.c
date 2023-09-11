eae4d89fea9e211bbc6d1018655fd92bf63e1d4c
cfranco87 <carlosfrancoart@gmail.com>
2023-09-11 14:50:59 +0000

    search binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,16 +12,25 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    long unsigned int i = 0;
-
-    if(!value && array == NULL)
+    int l = 0;
+    int r = size - 1;
+    int m = 0; 
+     
+    if(array == NULL || size == 0)
         return (-1);
 
     if (size == 0)
         return(-1);
-
     
-
-
-
+    while (l <= r) 
+    {
+        if (array[m] == value)
+            return (m); 
+        else if (array[m] < value)
+            return (l = m + 1);
+        else
+            return (r = m - 1);
+    }
+    
+    return(-1);
 }
